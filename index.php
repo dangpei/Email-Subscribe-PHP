1a67307a89abacf87fc2d86685df03a71f0594d6
Zhan Qian <daydream.qian@gmail.com>
2016-11-21 19:39:49 -0500

    初次提交，搭建前端页面

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Email Subscription</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+	<link rel="stylesheet" type="text/css" href="css/email-subscription.css" />
+</head>
+<body>
+
+
+	<main>
+
+
+
+		<form>
+			
+			<h1> Email Subscription</h1>
+
+			<br />
+			<br />
+
+			<div class="form-group wrapper">
+			    <label for="exampleInputEmail1">Email address</label>
+			    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+			    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+				
+				<button type="submit" class="btn btn-primary">Submit</button>
+
+		  	</div>
+			
+			
+
+		</form>
+		
+	</main>
+	
+</body>
+</html>
